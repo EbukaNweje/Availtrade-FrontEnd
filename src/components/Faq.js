469690f29038239b4e78546dd9d4d9515e3b3ba2
[Dinma-Faith] <[phaytep@gmail.com]>
2023-02-11 15:02:41 +0100

    Create Question component

---
+++ src/components/Faq.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { TiArrowSortedDown, TiArrowSortedUp } from "react-icons/ti";
+import "./Contact.css";
+import "./GetStarted.css";
+import "./Question.css";
+
+const Faq = (faqs) => {
+  const { faq, index } = faqs;
+  const [showAns, setshowAns] = useState(false);
+  const [showAns2, setshowAns2] = useState("");
+
+  const toggle = (id) => {
+    setshowAns(!showAns);
+    setshowAns2(id);
+  };
+  console.log(typeof showAns2, typeof faq.id);
+  return (
+    <div className="flex-faq">
+      <section className={"faq " + (faq.open ? "open" : "")} key={index}>
+        <div className="quest_arr">
+          <h4 className="faq_question" onClick={() => toggle(faq.id)}>
+            {faq.question}
+          </h4>
+
+          {showAns && showAns2 === faq.id ? (
+            <button className="faq-arrow">
+              <TiArrowSortedUp />
+            </button>
+          ) : (
+            <button>
+              <TiArrowSortedDown className="faq-arrow" />
+            </button>
+          )}
+        </div>
+        {/* {(+showAns2 === faq.id) ? <p className="faq_answer">{faq.answer} </p> : null} */}
+        {/* {showAns && (+showAns2 === faq.id) ? (console.log(faq.answer)) : null} */}
+        <br />
+        <div className="ans_arr">
+          <p>{showAns && showAns2 === faq.id ? faq.answer : null}</p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Faq;
---
+++ src/components/Question.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import {v4 as uuidv4} from 'uuid';
+import Faq from "./Faq";
+
+function Question() {
+  const [questions, setQuestions] = useState([]);
+  const allQuestions = [
+    {
+      id: uuidv4(),
+      question: 
+      "Q: What is the main idea behind the establishment of Availtrade.com? Is the company for ‘real’? And is there anything that really proves this?",
+      answer:
+      "A: Availtrade.com is a cryptocurrency trading company established in 2022. The wide array of services we offer were specifically designed to assist Bitcoin holders in making good trading decisions. We are a London-based company that possesses a group of trading professionals covering many industries, ranging from engineering to advanced computer technology. Our group of professionals are widely versed in cryptocurrency techniques as well and can expertly answer any question you might have concerning cryptocurrency trading.",
+      open: true,
+    },
+    {
+      id: uuidv4(),
+      question: "Q: You seem to provide a quite lucrative interest rate for a limited period of time. Where is the funds coming from?",
+      answer:
+        "A: As a result of the volume of cryptocurrency trading and our cutting-edge trading technology, we have the ability to provide an interest rate that’s higher than what’s usually obtainable in the average market. Moreover, the fact that we are always spreading our reach as far as we can globally helps us in reaching new heights. Plus, we have to stick only to concepts that have to do with modern-day infrastructure if we want to maintain our status as one of the best cryptocurrency trading platforms.",
+      open: false,
+    },
+    {
+      id: uuidv4(),
+      question: "Q: Are there risks involved?",
+      answer:
+        "A: You already made a good decision by deciding to join our community. Pipscountfx.com is a completely risk-free trading company, all thanks to our overall ideology and company concept. In addition, our company is made up of certified professionals in various fields such as cryptocurrency trading, blockchain technology, cryptocurrency finance and security. Our goal is to provide a seamless trading experience based on expert project management. We aim to make cryptocurrency trading available to anybody that has no experience within the field.",
+      open: false,
+    },
+  ];
+
+  useEffect(() => {
+    setQuestions(allQuestions);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div>
+      <section className="contact">
+        <h1 className="contact_h1 faq_h1">Frequently Asked Questions</h1>
+        <h3 className="faq_h3">
+          You ask, we answer. 99% of of most commonly asked questions are listed
+          here.
+        </h3>
+        <hr className="open_acct_hr faq_hr" />
+      </section>
+
+      <section className="sub-header-faq">
+        <span id="click-1">General</span>/
+        <span id="click-2">Account &amp; Transactions</span>/
+        <span id="click-3">Affiliate</span>
+        <br />
+        <br />
+      </section>
+
+      <section>
+        {questions.map((question, i) => (
+          <Faq faq={question} index={i} />
+        ))}
+      </section>
+    </div>
+  );
+}
+
+export default Question;
