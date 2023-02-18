343f76946db316885d9e874da762ebb999f8bddd
EbukaNweje <nwejeebuka@gmail.com>
2023-02-18 17:14:23 +0100

    Eslint fix

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import bg from '../asset/bitcoinwallpaper.jpg'
-import logo from '../asset/preeminentcryptotrade.png'
+import logo from '../asset/preeminentcryptotrade.png';
 
 function SignUp({Display}) {
 
